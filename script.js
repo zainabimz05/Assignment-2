f49a1739ef59b275943f029b2f2d625fe32e475f
Zainab <zainabimz05@gmail.com>
2026-07-14 01:15:26 +0500

    Add smooth scrolling navigation

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for navigation links
+
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
